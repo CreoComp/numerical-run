bbb4b553b46cceb0ff55fb90152f154e16e26b52
Rezak <a.grek72@yahoo.com>
2023-06-30 21:11:34 +0300

    Убрал UI осколков с игровой сцены

--- Assets/Scripts/GameManager/GameState.cs
+++ Assets/Scripts/GameManager/GameState.cs
@@ -28,7 +28,6 @@ public class GameState : AState
 
     [Header("UI")]
     public Text coinText;
-    public Text fragmentsText;
     public Text scoreText;
 	public Text distanceText;
     public Text multiplierText;
@@ -330,7 +329,6 @@ public class GameState : AState
     protected void UpdateUI()
     {
         coinText.text = PlayerData.instance.Coins.ToString();
-        fragmentsText.text = PlayerData.instance.Fragments.ToString();
 
         scoreText.text = trackManager.score.ToString();
         multiplierText.text = "x " + trackManager.multiplier;
